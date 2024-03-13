0f56267c96aec127f2e9aae3aa9380c77de5a209
jayarekhamunjuluru <163150033+jayarekhamunjuluru@users.noreply.github.com>
2024-03-13 15:13:56 +0530

    Update project4.py

--- project4.py
+++ project4.py
@@ -9,4 +9,4 @@ def generate_password(length=12):
 if __name__ == "__main__":
     password_length = int(input("Enter the length of the password: "))
     generated_password = generate_password(password_length)
-    print("Generated Password:", generated_password)16
+    print("Generated Password:", generated_password)
